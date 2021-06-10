efe9ed5845e82cbfaf23406d968daa94a897b676
Matt Rudge <matt.rudge@codeinstitute.net>
2021-06-10 16:13:09 +0000

    add commenting

--- 09_commenting/codestar/settings.py
+++ 09_commenting/codestar/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     'cloudinary_storage',
     'django.contrib.staticfiles',
     'cloudinary',
+    'crispy_forms',
     'allauth',
     'allauth.account',
     'allauth.socialaccount',

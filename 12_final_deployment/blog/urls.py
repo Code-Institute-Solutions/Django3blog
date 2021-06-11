d6c81c03e84eb3966241906eebeaa29a27e202dd
Matt Rudge <matt.rudge@codeinstitute.net>
2021-06-11 08:40:04 +0000

    update templates

---
+++ 12_final_deployment/blog/urls.py
@@ -0,0 +1,8 @@
+from . import views
+from django.urls import path
+
+urlpatterns = [
+    path('', views.PostList.as_view(), name='home'),
+    path('<slug:slug>/', views.PostDetail.as_view(), name='post_detail'),
+    path('like/<slug:slug>', views.PostLike.as_view(), name='post_like'),
+]
